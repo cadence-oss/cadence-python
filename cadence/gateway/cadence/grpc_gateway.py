9dc4186b3e5c71cad2643424c46abe4d896fded1
Rodrigo Villarreal <noiarek@gmail.com>
2021-11-08 19:07:06 -0800

    Implementing interfaces for grpc and tchannel client

---
+++ cadence/gateway/cadence/grpc_gateway.py
@@ -0,0 +1,30 @@
+from typing import Tuple
+import grpc
+import uber.cadence.api.v1.service_domain_pb2_grpc as service_domain_pb2_grpc
+from cadence.cadence_types import ListDomainsRequest, ListDomainsResponse
+from cadence.gateway.cadence.interface import CadenceGatewayInterface
+
+
+class GRPCGateway(CadenceGatewayInterface):
+    """
+    Client for gRPC functionality
+    """
+    metadata = [('rpc-caller', 'python-client'), ('rpc-service', 'cadence-frontend'), ('rpc-encoding', 'proto')]
+
+    def __init__(self, host: str = 'localhost', port: int = 7833, timeout: int = 300):
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+        self.channel = grpc.insecure_channel(
+            '{}:{}'.format(host, port))
+
+        # stubs
+        self.domain = service_domain_pb2_grpc.DomainAPIStub(self.channel)
+
+    def list_domains(self, request: ListDomainsRequest) -> Tuple[ListDomainsResponse, object]:
+        response = self.domain.ListDomains.with_call(
+            request,
+            metadata=self.metadata,
+            timeout=self.timeout
+        )
+        return response[0]
