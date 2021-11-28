518dc698b85b4190afff31990cd8e6619c3d539b
Rodrigo Villarreal <noiarek@gmail.com>
2021-11-27 20:54:32 -0800

    Deleting comment and adding extra sample

--- cadence/mapping/grpc/service_workflow.py
+++ cadence/mapping/grpc/service_workflow.py
@@ -1,25 +1,3 @@
-"""
-Will delete after modeling all the fields
-
-message StartWorkflowExecutionRequest {
-  string domain = 1;
-  string workflow_id = 2;
-  WorkflowType workflow_type = 3;
-  TaskList task_list = 4;
-  Payload input = 5;
-  google.protobuf.Duration execution_start_to_close_timeout = 6;
-  google.protobuf.Duration task_start_to_close_timeout = 7;
-  string identity = 8;
-  string request_id = 9;
-  WorkflowIdReusePolicy workflow_id_reuse_policy = 10;
-  RetryPolicy retry_policy = 11;
-  string cron_schedule = 12;
-  Memo memo = 13;
-  SearchAttributes search_attributes = 14;
-  Header header = 15;
-  google.protobuf.Duration delay_start = 16;
-}
-"""
 from cadence.cadence_types import StartWorkflowExecutionRequest, WorkflowType, TaskList, TaskListKind, \
     WorkflowIdReusePolicy, RetryPolicy, Memo, SearchAttributes, Header, StartWorkflowExecutionResponse
 
