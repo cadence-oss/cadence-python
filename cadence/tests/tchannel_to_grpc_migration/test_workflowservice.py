d0e79284cc2f45d552f8c8f1f64bb3c4cfd7dbc9
Rodrigo Villarreal <rodrigo.villarreal@uber.com>
2022-01-20 07:16:08 -0800

    update domain grpc

--- cadence/tests/tchannel_to_grpc_migration/test_workflowservice.py
+++ cadence/tests/tchannel_to_grpc_migration/test_workflowservice.py
@@ -4,7 +4,7 @@ import time
 
 from cadence.gateway.cadence.enums import DefaultPort, ConnectionProtocol
 from cadence.cadence_types import StartWorkflowExecutionRequest, TaskList, WorkflowType, \
-    RegisterDomainRequest, DescribeDomainRequest
+    RegisterDomainRequest, DescribeDomainRequest, UpdateDomainRequest, DomainConfiguration
 from cadence.workflowservice import WorkflowService
 
 
@@ -25,6 +25,19 @@ class TestStartWorkflow(TestCase):
         response, err = self.service.describe_domain(request)
         self.assertIsNone(err)
         self.assertIsNotNone(response)
+
+    def test_update_domain(self):
+        register_request = RegisterDomainRequest()
+        register_request.name = str(uuid4())
+        self.service.register_domain(register_request)
+        request = UpdateDomainRequest()
+        request.name = register_request.name
+        request.configuration = DomainConfiguration()
+        request.configuration.workflow_execution_retention_period_in_days = 10
+        response, err = self.service.update_domain(request)
+        self.assertIsNone(err)
+        self.assertIsNotNone(response)
+        self.assertEqual(10, response.configuration.workflow_execution_retention_period_in_days)
 
 
     def tearDown(self) -> None:
